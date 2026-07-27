d61b2eda565d908cc014ac5bfe6dba9756504e37
Shweta Bhange <shwetagb1013@gamil.com>
2026-07-27 16:58:30 +0530

    Latest changes

--- Calculator.java
+++ Calculator.java
@@ -2,14 +2,14 @@ public class Calculator {
 
     public static void main(String[] args) {
 
-        int a = 50;
-        int b = 10;
-        int c = 40;
+        int a = 20;
+        int b = 15;
+        int c = 70;
 
-        System.out.println("Addition = " + (a + b));
-        System.out.println("Subtraction = " + (a - b));
-        System.out.println("Multiplication = " + (a * b));
-        System.out.println("Division = " + (a / b));
+        System.out.println("Addition shweta = " + (a + b +c));
+        System.out.println("Subtraction = " + (a - b -c));
+        System.out.println("Multiplication = " + (a * b *c));
+        System.out.println("Division = " + (a / b /c));
         // first comment 
         //Second comment added by Gajanan
     }
