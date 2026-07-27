48639c2920736026db44a6f67ff5578df6e2bfee
Gajanan <gajananbhange200@gmail.com>
2026-07-27 16:45:14 +0530

    Modify variable 'a' and add a comment
    Updated variable 'a' to 50 and added a new comment.

--- Calculator.java
+++ Calculator.java
@@ -2,14 +2,16 @@ public class Calculator {
 
     public static void main(String[] args) {
 
-        int a = 30;
+        int a = 50;
         int b = 10;
+        int c = 40;
 
         System.out.println("Addition = " + (a + b));
         System.out.println("Subtraction = " + (a - b));
         System.out.println("Multiplication = " + (a * b));
         System.out.println("Division = " + (a / b));
         // first comment 
+        //Second comment added by Gajanan
     }
 
 }
