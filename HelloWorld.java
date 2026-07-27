a6887b2a95046eb2bf8416d3cbc5940518433c6d
Shweta Bhange <shwetagb1013@gamil.com>
2026-07-27 16:12:59 +0530

    Modified with new changes HelloWordJava and New file added Student.Java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
         System.out.println("Hello Git!");
         System.out.println("My name is Shweta Bhange.");
         System.out.println("I am learning Git and GitHub.");
+         System.out.println("new code");
 
     }
 
---
+++ Student.java
@@ -0,0 +1,15 @@
+public class Student {
+
+    public static void main(String[] args) {
+
+        String name = "Shweta";
+        int age = 22;
+        double marks = 89.5;
+
+        System.out.println("----- Student Details -----");
+        System.out.println("Name  : " + name);
+        System.out.println("Age   : " + age);
+        System.out.println("Marks : " + marks);
+
+    }
+}
