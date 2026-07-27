1a49b4426d7cfa660f5a16ce9b6a7d11b2f0891a
Shweta Bhange <shwetagb1013@gamil.com>
2026-07-27 15:54:01 +0530

    Changes to be committed:     new file: HelloWorld.java     new file: Calculator.java

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Git!");
+        System.out.println("My name is Shweta Bhange.");
+        System.out.println("I am learning Git and GitHub.");
+
+    }
+
+}
