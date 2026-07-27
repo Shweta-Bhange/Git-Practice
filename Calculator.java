e31cf161d2fc7dbac0a8826b5fa2220f110005e2
Gajanan <gajananbhange200@gmail.com>
2026-07-27 16:57:09 +0530

    Update variables and output message in Calculator

--- Calculator.java
+++ Calculator.java
@@ -2,11 +2,11 @@ public class Calculator {
 
     public static void main(String[] args) {
 
-        int a = 50;
+        int a = 80;
         int b = 10;
-        int c = 40;
+        int c = 80;
 
-        System.out.println("Addition = " + (a + b));
+        System.out.println("Addition Gajanan= " + (a + b));
         System.out.println("Subtraction = " + (a - b));
         System.out.println("Multiplication = " + (a * b));
         System.out.println("Division = " + (a / b));
